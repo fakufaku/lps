b8f76bd5825e4c4b0c251dc6c30af7cd4f497528
Pirklbauer <jan.pirklbauer@tu-bs.de>
2024-01-09 11:53:07 +0100

    Changed package directory

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="levenshtein-phoneme-similarity",
-    version="0.0.1",
+    version="0.0.2",
     author="Jan Pirklbauer",
     author_email="jan.pirklbauer@tu-bs.de",
     description="Implementation of the Levneshtein Phoneme Distance",
+    package_dir={"": "src"},
     packages=find_packages(),
     install_requires=[
         'levenshtein',
