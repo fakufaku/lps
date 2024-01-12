9f3872a25a3d126f208547c6dff5f09518f4ac76
Pirklbauer <jan.pirklbauer@tu-bs.de>
2024-01-12 11:27:50 +0100

    corrected test paths

--- tests/test_levenshtein.py
+++ tests/test_levenshtein.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
 from Levenshtein import distance
 from lps.lps import LevenshteinPhonemeSimilarity
-import torchaudio
 import soundfile as sf
 
 
@@ -10,9 +9,8 @@ class TestLevenshtein(TestCase):
         self.assertEqual(distance("foo", "nooo"), 2)
 
     def test_levenshtein_sim(self):
-        (ref, _), (sample, _) = torchaudio.load("resources/speech.wav"), torchaudio.load("resources/speech_bab_0dB.wav")
+        (ref, _), (sample, _) = sf.read("tests/resources/speech.wav"), sf.read("tests/resources/speech_bab_0dB.wav")
         lps = LevenshteinPhonemeSimilarity()
-        ref = ref.numpy()
-        sample = sample.numpy()
         self.assertEqual(lps(ref, ref), 1.0)
-        self.assertLess(lps(ref, sample), 1.0)
+        self.assertLess(lps(sample, ref), 1.0)
+        print(lps(sample, ref))
